bf774126e914cdbe21dcc6c13e4046e545a2b141
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-06 14:07:51 +0300

    create and add components

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -13,7 +13,25 @@ class Calculator extends React.Component {
         <div id="result" className="result-display calc-button-style">
           {result}
         </div>
-        
+        <div className="calc-button-style">AC</div>
+        <div className="calc-button-style">+/-</div>
+        <div className="calc-button-style">%</div>
+        <div className="operators-buttons calc-button-style">÷</div>
+        <div className="calc-button-style">7</div>
+        <div className="calc-button-style">8</div>
+        <div className="calc-button-style">9</div>
+        <div className="operators-buttons calc-button-style">x</div>
+        <div className="calc-button-style">4</div>
+        <div className="calc-button-style">5</div>
+        <div className="calc-button-style">6</div>
+        <div className="operators-buttons calc-button-style">-</div>
+        <div className="calc-button-style">1</div>
+        <div className="calc-button-style">2</div>
+        <div className="calc-button-style">3</div>
+        <div className="operators-buttons calc-button-style">+</div>
+        <div className="zero-button calc-button-style">0</div>
+        <div className="calc-button-style">.</div>
+        <div className="operators-buttons calc-button-style">=</div>
       </div>
     );
   }
