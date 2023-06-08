ea41cbfe48b9199848d41c5282ae61b634cd845b
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-08 19:30:06 +0300

    Create a new component to display qoutes

---
+++ src/components/displayQuoate.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+
+const API_URL = 'https://api.api-ninjas.com/v1/quotes?category = computers';
+const GetQuotes = () => {
+  
+};
+
+export default GetQuotes;
