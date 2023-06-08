9aeb132c019afcd3f0d40a4dd4b50a83aa7e84b5
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-08 19:31:29 +0300

    Add state variables

--- src/components/displayQuoate.js
+++ src/components/displayQuoate.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from 'react';
 
 const API_URL = 'https://api.api-ninjas.com/v1/quotes?category = computers';
 const GetQuotes = () => {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+  const [error, setError] = useState('');
+  const [isLoading, setLoading] = useState(true);
+
   
 };
 
