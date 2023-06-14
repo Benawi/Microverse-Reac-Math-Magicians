aa642d627707883945001c3841643fe9cb1f65f1
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-14 11:56:55 +0300

    Add test case for substract operator

--- src/test/operate.test.js
+++ src/test/operate.test.js
@@ -7,4 +7,10 @@ describe('add', () => {
   });
 });
 
+describe('subtract', () => {
+  test('subtracts two numbers correctly', () => {
+    expect(operate(5, 3, '-')).toBe('2');
+    expect(operate(1, 1, '-')).toBe('0');
+  });
 });
+
