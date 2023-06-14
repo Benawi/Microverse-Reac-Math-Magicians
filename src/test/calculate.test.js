af6f2fa5c266bf22987b74d006c2a5029c1dadd7
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-14 13:51:59 +0300

    Add test case for calculate when no numbers

--- src/test/calculate.test.js
+++ src/test/calculate.test.js
@@ -5,12 +5,12 @@ describe('calculate', () => {
     const result = calculate({}, 'AC');
     expect(result).toEqual({ total: null, next: null, operation: null });
   });
-
-  test('updates "next" with a number when no operation is set', () => {
+ test('updates "next" with a number when no operation is set', () => {
     const obj = { next: '5', total: null, operation: null };
     const result = calculate(obj, '2');
     expect(result).toEqual({ next: '52', total: null });
   });
+ 
 
  
 });
